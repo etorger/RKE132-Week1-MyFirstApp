23070cb36bf5ecafbd067de20b9615457ad230e4
etorger <etorger@tktk.ee>
2022-09-07 13:29:12 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿//rakendus küsib kasutaja nime
+//rakesndus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne(sõna?) 
+string userName = Console.ReadLine(); //input
+Console.WriteLine("Hello " + userName + "!"); //output
+
+ 
+
+//string inerpolation
+Console.WriteLine($"Hello, userName ! "); //output
+//output=kõik mida me kuvame on output ja sisestame on input(username)
+
+
